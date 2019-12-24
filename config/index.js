1e4e22226e3009e84be678f486610bbaa24d5057
陈大仙 <13288651469@163.com>
2019-12-24 10:33:52 +0800

    '关于config问价夹详情'

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 'use strict'
+// 整个项目主要的配置入口
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
@@ -6,16 +7,26 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    // 需要代理的接口，可以跨域
     proxyTable: {},
-
+    /*
+     proxyTable: {
+        '/shishuiyuan': {
+            target: 'http://rz.shiwen123.com/',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/shishuiyuan': ''
+            }
+        }
+     },
+    * */
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
@@ -45,11 +56,15 @@ module.exports = {
 
   build: {
     // Template for index.html
+    // 编译输入的index.html文件。node.js中，在任何模块文件内部，可以使用__filename变量获取当前模块文件的带有完整绝对路径的文件名,
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
+    // 编译输出的静态资源路径
     assetsRoot: path.resolve(__dirname, '../dist'),
+    // 编译输出的二级目录
     assetsSubDirectory: 'static',
+    // 编译发布的根目录，可配置为资源服务器或者cdn域名
     assetsPublicPath: '/',
 
     /**
